e346f66cf2ae1d2a51f212a2c50b79b583dacb0d
Div-VT <div@voicetube.com>
2018-03-24 02:40:07 +0800

    inrtroductions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,13 @@
 var app = new Vue({
     el: '#app',
     data: {
+        introductions: [
+            '3 years of work experience in web development.',
+            'Familiar with Laravel development.',
+            'Familiar with wordpress develop. Can customized theme and code with wp formal method.',
+            'Front-End and Back-End skills.',
+            'Good communication skills and patience',
+        ],
         skills: [
             'PHP',
             'Laravel/Lumen',
