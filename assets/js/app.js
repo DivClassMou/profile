9eb7f1b89e59c50e61384713653da64cfe6f4587
Div-VT <div@voicetube.com>
2018-03-24 01:59:15 +0800

    introduction and skills section

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        skills: [
+            'PHP',
+            'Laravel/Lumen',
+            'Node JS',
+            'HTML',
+            'CSS/SASS/SCSS',
+            'Javascript',
+            'jQuery',
+            'Vue',
+            'Underscore/Lodash',
+            'Bootstrap/Semantic UI/Materialize',
+            'Git',
+            'Wordpress',
+        ],
+    }
+})
